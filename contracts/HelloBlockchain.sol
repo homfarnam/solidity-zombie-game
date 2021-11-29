ac3d0476b84462d3c1cf9f902e717d257742e91d
homfarnam <homfarnam@gmail.com>
2021-11-29 18:32:04 +0330

    Change func to internal

--- contracts/HelloBlockchain.sol
+++ contracts/HelloBlockchain.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.4.20;
 
 contract ZombieFactory {
   event NewZombie(uint256 zombieId, string name, uint256 dna);
+
   uint256 dnaDigits = 16;
   uint256 dnaModulus = 10**dnaDigits;
 
@@ -14,9 +15,17 @@ contract ZombieFactory {
 
   Zombie[] public zombies;
 
-  function _createZombie(string memory _name, uint256 _dna) private {
+  mapping(uint256 => address) public zombieToOwner;
+  mapping(address => uint256) ownerZombieCount;
+
+  function _createZombie(string memory _name, uint256 _dna) internal {
     zombies.push(Zombie(_name, _dna));
     uint256 id = zombies.length - 1;
+
+    zombieToOwner[id] = msg.sender;
+
+    ownerZombieCount[msg.sender]++;
+
     emit NewZombie(id, _name, _dna);
   }
 
@@ -26,9 +35,9 @@ contract ZombieFactory {
   }
 
   function createRandomZombie(string memory _name) public {
+    require(ownerZombieCount[msg.sender] == 0);
+
     uint256 randDna = _generateRandomDna(_name);
     _createZombie(_name, randDna);
   }
-
-  // start here
 }
