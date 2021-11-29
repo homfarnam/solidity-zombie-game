8aaec6612dd5ed0c5be4d005f4645b19af0ddca4
homfarnam <homfarnam@gmail.com>
2021-11-29 11:21:45 +0330

    Create new functions

--- contracts/HelloBlockchain.sol
+++ contracts/HelloBlockchain.sol
@@ -13,8 +13,19 @@ contract ZombieFactory {
 
   Zombie[] public zombies;
 
-  function createZombie(string memory _name, uint256 _dna) public {
-    // start here
+  function _createZombie(string memory _name, uint256 _dna) private {
     zombies.push(Zombie(_name, _dna));
   }
+
+  function _generateRandomDna(string memory _str) private view returns (uint256) {
+    uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
+    return rand % dnaModulus;
+  }
+
+  function createRandomZombie(string memory _name) public {
+    uint256 randDna = _generateRandomDna(_name);
+    _createZombie(_name, randDna);
+  }
+
+  // start here
 }
