a8a701bac4be8b9100de65735e293ea452021e92
homfarnam <homfarnam@gmail.com>
2021-11-30 11:57:13 +0330

    Create subcurrency contract

---
+++ contracts/subcurrency.sol
@@ -0,0 +1,33 @@
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Subcurrency {
+  address public minter;
+
+  mapping(address => uint256) public balances;
+
+  // modifiers
+
+  modifier onlyMinter() {
+    require(msg.sender == minter, 'Only minter can call this function!');
+    _;
+  }
+
+  event Sent(address from, address to, uint256 amount);
+
+  constructor() public {
+    minter = msg.sender;
+  }
+
+  function mint(address receiver, uint256 amount) public onlyMinter {
+    balances[receiver] += amount;
+  }
+
+  function sender(address receiver, uint256 amount) public {
+    if (amount < balances[msg.sender]) revert('insufficent balance!');
+
+    balances[msg.sender] -= amount;
+    balances[receiver] += amount;
+
+    emit Sent(msg.sender, receiver, amount);
+  }
+}
