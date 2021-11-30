3e1f17bff49530ab7831f505e6430ce3495c740a
homfarnam <homfarnam@gmail.com>
2021-11-30 11:57:24 +0330

    Add new functions

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -1,12 +1,10 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.20;
+pragma solidity >=0.5.0 <0.9.0;
 import './HelloBlockchain.sol';
 
-abstract contract KittyInterface {
+contract KittyInterface {
   function getKitty(uint256 _id)
     external
     view
-    virtual
     returns (
       bool isGestating,
       bool isReady,
@@ -22,10 +20,20 @@ abstract contract KittyInterface {
 }
 
 contract ZombieFeeding is ZombieFactory {
-  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-  KittyInterface kittyContract = KittyInterface(ckAddress);
+  KittyInterface kittyContract;
 
-  // Modify function definition here:
+  function setKittyContractAddress(address _address) external onlyOwner {
+    kittyContract = KittyInterface(_address);
+  }
+
+  function _triggerCooldown(Zombie storage _zombie) internal {
+    _zombie.readyTime = uint32(block.timestamp + cooldownTime);
+  }
+
+  function _isReady(Zombie storage _zombie) internal view returns (bool) {
+    return (_zombie.readyTime <= now);
+  }
+
   function feedAndMultiply(
     uint256 _zombieId,
     uint256 _targetDna,
