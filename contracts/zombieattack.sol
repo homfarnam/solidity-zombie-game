f2d9e7fbfc927d12451196ba73547ac2203f0c4f
homfarnam <homfarnam@gmail.com>
2021-12-02 23:27:41 +0330

    Add attack configs

--- contracts/zombieattack.sol
+++ contracts/zombieattack.sol
@@ -11,5 +11,21 @@ contract ZombieAttack is ZombieHelper {
     return uint256(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
   }
 
-  function attack(uint256 _zombieId, uint256 _targetId) external {}
+  function attack(uint256 _zombieId, uint256 _targetId) external ownerOf(_zombieId) {
+    Zombie storage myZombie = zombies[_zombieId];
+    Zombie storage enemyZombie = zombies[_targetId];
+
+    uint256 rand = randMod(100);
+
+    if (rand <= attackVictoryProbability) {
+      myZombie.winCount++;
+      myZombie.level++;
+      enemyZombie.lossCount++;
+      feedAndMultiply(_zombieId, enemyZombie.dna, 'zombie');
+    } else {
+      myZombie.lossCount++;
+      enemyZombie.winCount++;
+      _triggerCooldown(myZombie);
+    }
+  }
 }
