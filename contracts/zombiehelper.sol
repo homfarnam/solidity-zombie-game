4640eabb85d880319fdfc86b2176ce6c3a6f99cd
homfarnam <homfarnam@gmail.com>
2021-12-02 23:28:20 +0330

    Add owner

--- contracts/zombiehelper.sol
+++ contracts/zombiehelper.sol
@@ -29,13 +29,16 @@ contract ZombieHelper is ZombieFeeding {
   function changeName(uint256 _zombieId, string calldata _newName)
     external
     aboveLevel(2, _zombieId)
+    ownerOf(_zombieId)
   {
-    require(msg.sender == zombieToOwner[_zombieId]);
     zombies[_zombieId].name = _newName;
   }
 
-  function changeDna(uint256 _zombieId, uint256 _newDna) external aboveLevel(20, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
+  function changeDna(uint256 _zombieId, uint256 _newDna)
+    external
+    aboveLevel(20, _zombieId)
+    ownerOf(_zombieId)
+  {
     zombies[_zombieId].dna = _newDna;
   }
 
