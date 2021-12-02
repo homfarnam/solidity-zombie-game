a5471f33e63eb8b90518888efd2e06c0a95344bb
homfarnam <homfarnam@gmail.com>
2021-12-03 01:53:20 +0330

    Change increase functions

--- contracts/zombieattack.sol
+++ contracts/zombieattack.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.5.0 <0.6.0;
+pragma solidity >=0.4.0;
 import './zombiehelper.sol';
 
 contract ZombieAttack is ZombieHelper {
@@ -7,7 +7,7 @@ contract ZombieAttack is ZombieHelper {
   uint256 attackVictoryProbability = 70;
 
   function randMod(uint256 _modulus) internal returns (uint256) {
-    randNonce++;
+    randNonce = randNonce.add(1);
     return uint256(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
   }
 
@@ -18,13 +18,13 @@ contract ZombieAttack is ZombieHelper {
     uint256 rand = randMod(100);
 
     if (rand <= attackVictoryProbability) {
-      myZombie.winCount++;
-      myZombie.level++;
-      enemyZombie.lossCount++;
+      myZombie.winCount = myZombie.winCount.add(1);
+      myZombie.level = myZombie.level.add(1);
+      enemyZombie.lossCount = enemyZombie.lossCount.add(1);
       feedAndMultiply(_zombieId, enemyZombie.dna, 'zombie');
     } else {
-      myZombie.lossCount++;
-      enemyZombie.winCount++;
+      myZombie.lossCount = myZombie.lossCount.add(1);
+      enemyZombie.winCount = enemyZombie.winCount.add(1);
       _triggerCooldown(myZombie);
     }
   }
